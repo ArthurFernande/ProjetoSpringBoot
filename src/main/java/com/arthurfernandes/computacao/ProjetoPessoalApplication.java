ca00c1809f34d8ad997c4c5bcf9deef41953f5e5
ArthurFernande <arthur.andrade@ccc.ufcg.edu.br>
2021-12-21 14:19:50 -0300

    Projeto criado

---
+++ src/main/java/com/arthurfernandes/computacao/ProjetoPessoalApplication.java
@@ -0,0 +1,13 @@
+package com.arthurfernandes.computacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoPessoalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoPessoalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arthurfernandes/computacao/ProjetoPessoalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arthurfernandes.computacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoPessoalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
